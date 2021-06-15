862d053385cac539f4bbf9416ed2a07af51a37a7
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-15 19:38:50 +0530

    Added: Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,17 +1,20 @@
-public class EmpWage
-{
-    public static void main(String args[])
-    {
-        final int FULL_TIME = 1;
-        int empType = (int) (Math.random() * 100) % 2;
-        if (empType == FULL_TIME)
-        {
-            System.out.println("Employee is Present");
-
-        } else
-        {
-            System.out.println("Employee is Absent");
-
-        }
-    }
+public class EmpWage{
+	public static void main(String[] args){
+		
+		int Full_Time=1;
+		int Wage_per_hr=20;
+		int Full_Day_hr=8;
+		
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == Full_Time)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Abscent");
+		}
+		
+	}
+	
 }

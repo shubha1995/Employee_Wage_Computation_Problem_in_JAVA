3a64dce63e21c239273de46722eca06f44085212
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-15 13:36:05 +0530

    Added: uc1 Check Employee is

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,17 @@
-public class EmpWage{
-	public static void main(String[] args){
-	System.out.println("Welcome to Employee Wage Computation Problem");
-	}
-      }	
+public class EmpWage
+{
+    public static void main(String args[])
+    {
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
+    }
+}

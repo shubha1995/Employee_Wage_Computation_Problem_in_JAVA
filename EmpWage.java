1ff2a68775b5f09e4fe926e13bb0eb32fb0c3f40
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-18 09:13:59 +0530

    Added: Calculate Wage till reach 100 hour or 20 days

--- EmpWage.java
+++ EmpWage.java
@@ -1,31 +1,44 @@
-public class EmpWage
-{
-    public static void main(String args[])
-    {
-         final int Part_Time = 1;
-         final int Full_Time = 2;
-         final int Wage_Per_hr = 20;
-         final int Working_Days = 20;
-    
-        int totalWage = 0;
-        for (int day = 1; day <= Working_Days; day++)
-        {
-            int empType = (int) (Math.random() * 10) % 3;
-            int Working_Hours =0;
-            switch (empType)
-            {
-                case Full_Time:
-                    Working_Hours = 8;
-                    break;
-                case Part_Time:
-                    Working_Hours = 4;
-                    break;
-                default:
-            }
-            int wage = Working_Hours * Wage_Per_hr;
-            System.out.println("Day " + day + " wage is:" + wage);
-            totalWage += wage;
-        }
-        System.out.println("Total wage for a month is " + totalWage);        
-    }
+public class EmpWage{
+
+	public static void main(String[] args) {
+
+		
+		int FullDayHr=8;
+		int PartTimeHr=4;
+		int WagePerHr=20;
+		int day=0;
+		int empHr=0;
+		int wage = 0;
+		//int n;
+		while (day <= 19) {
+			int n = (int) (Math.random() * 10) % 3;
+		switch (n) {
+		case 1:
+			System.out.println("present fullday");
+			wage = (wage + (WagePerHr * FullDayHr));
+			//System.out.println("fulltime emp wage"+wage);
+            empHr=(empHr+ FullDayHr);
+			day++;
+			break;
+		case 2:
+			System.out.println("present PartTime");
+			wage = (wage + (WagePerHr * PartTimeHr));
+			//System.out.println("PartTime emp wage"+wage);
+			empHr=(empHr+ PartTimeHr);
+			
+			day++;
+			break;
+		default:
+			System.out.println("absent");
+		    day++;
+			if( empHr == 100)
+				break;
+		}
+		 }
+		System.out.println("total wage of a month is");
+		System.out.println(wage);
+		System.out.println(empHr);
+		System.out.println(day);
+
+	}
 }

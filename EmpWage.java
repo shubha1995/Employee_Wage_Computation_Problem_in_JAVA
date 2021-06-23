18cbd570ae5790db51e5b5f6656f42d6794236e5
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-23 07:38:38 +0530

    Added: Compute Employee Wage For Multiple Company

--- EmpWage.java
+++ EmpWage.java
@@ -1,32 +1,35 @@
 
-
 public class EmpWage{
 
-	public static void calculateWage() {
+	public static void calculateWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
 
 		
-		int FullDayHr=8;
-		int PartTimeHr=4;
-		int WagePerHr=20;
+		int FULL_DAYHR=8;
+		int PART_TIMEHR=4;
 		int day=0;
 		int empHr=0;
 		int wage = 0;
-		//int n;
+		System.out.println("Details of " + companyName + " employee");
+        System.out.println("|.......................................|");
+        System.err.println("Wage per hour:" + wagePerHr);
+        System.out.println("Maximum working days:" + maxWorkingDays);
+        System.out.println("Maximum working hours:" + maxWorkingHrs);																		
+		
 		while (day <= 19) {
 			int n = (int) (Math.random() * 10) % 3;
 		switch (n) {
 		case 1:
 			System.out.println("present fullday");
-			wage = (wage + (WagePerHr * FullDayHr));
-			//System.out.println("fulltime emp wage"+wage);
-            empHr=(empHr+ FullDayHr);
+			wage = (wage + (maxWorkingHrs * FULL_DAYHR));
+			
+            empHr=(empHr+ FULL_DAYHR);
 			day++;
 			break;
 		case 2:
 			System.out.println("present PartTime");
-			wage = (wage + (WagePerHr * PartTimeHr));
-			//System.out.println("PartTime emp wage"+wage);
-			empHr=(empHr+ PartTimeHr);
+			wage = (wage + (maxWorkingHrs * PART_TIMEHR));
+			
+			empHr=(empHr+ PART_TIMEHR);
 			
 			day++;
 			break;
@@ -40,11 +43,13 @@ public class EmpWage{
 		System.out.println("total wage of a month is: " +wage);
 		
 		System.out.println("Employee Hour: "+empHr);
-		System.out.println("Employee Work Day: "+day);
+		System.out.println("Employee Work Day: "+day + "\n");
 
 	}
 	public static void main(String[] args) {
-		calculateWage();
+
+        calculateWage("FlipKart", 50, 20, 200);
+        calculateWage("Amazon", 25, 25, 250);
 		
 	}
 }
